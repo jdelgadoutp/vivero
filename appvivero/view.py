710af6c6349b73dae1faf67cb6814c9991cede11
anibal0531 <anibal.toro@utp.edu.co>
2022-05-29 22:12:02 -0500

    se implementa logica de Productorview

--- appvivero/view.py
+++ appvivero/view.py
@@ -1,11 +1,27 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-#from .forms import LaborForm
-#from django.core.urlresolvers import reverse_lazy
 from .models import Procutor
 from django.views.generic import CreateView
 
-# Create your views here.
-def laborview(request):
-
+def Productorview(request):
     return render(request, 'ModuloLabor/registrolabor.html')
+
+    class Productor(CreateView):
+        model = ModeloLaborVivero
+        form_class = LaborForm
+
+        def get_context_data(self, **kwargs):
+            context = super(Procutor, self).get_context_data(**kwargs)
+            if 'form' not in context:
+                context['form'] = self.form_class(self.request.GET)
+                return context
+
+        def post(self, request, *args, **kwargs):
+            self.object = self.get_object
+            form = self.form_class(request.POST)
+            if form.is_valid():
+                solicitud = form.save(commit=False)
+                solicitud.save()
+                return HttpResponseRedirect(self.get_success_url())
+            else:
+                return self.render_to_response(self.get_context_data(form=form))
