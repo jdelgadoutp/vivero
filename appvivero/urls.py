bd61a3f654c83e52b1e53cc3073fb9e33c48249e
anibal0531 <anibal.toro@utp.edu.co>
2022-05-29 22:18:35 -0500

    se implementan las url

--- appvivero/urls.py
+++ appvivero/urls.py
@@ -1,21 +1,19 @@
-"""appvivero URL Configuration
+from django.test import TestCase
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+# Create your tests here.
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
+from appvivero import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.ViveroCreateView.as_view(), name='viverohome'),  #as_view() es porque estamos trabajando con vista basada en clases
+    # path('registrovivero/', views.viveroview, name='registro_vivero'),
+    path('registrovivero/', views.ViveroCreateView.as_view(), name='registro_vivero'),
+    #Lista viveros
+    path('listarviveros/', views.ViveroListarView.as_view(), name='listar_vivero'),
+    path('editarviveros/<int:pk>/', views.ViveroEditar.as_view(), name='editar_vivero'),
+    path('eliminarvivero/<int:pk>/', views.ViveroEliminar.as_view(), name='eliminar_vivero'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
